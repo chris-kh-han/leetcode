6b87014827074769e212cea45f8b0a5182ca732b
Chris KH Han <chris.kh.han@gmail.com>
2021-01-18 22:44:56 -0600

    update

---
+++ 7_reverseInteger.js
@@ -0,0 +1,11 @@
+var reverse = function(x) {
+    
+    let reverse = parseInt(String(x).split("").reverse().join(""));
+
+    if (reverse >= -(2**31) && reverse >= 2**31) return 0 
+
+    return reverse
+
+};
+
+console.log(reverse(-123));
