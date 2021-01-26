9312c57b6b61a0b89929e52f525573cde7ebdb20
Chris KH Han <chris.kh.han@gmail.com>
2021-01-26 10:28:41 -0600

    update

---
+++ 204_countPrime.js
@@ -0,0 +1,18 @@
+var countPrimes = function(n) {
+    let count = 0;
+    let arr = [];
+    for (let i = 2; i < n; i++) {
+        if (arr[i]) {
+            
+            continue;
+        }
+        
+        count++
+        for (let j = i + i; j < n; j += i) {
+            
+            arr[j] = true;
+        }
+    }
+
+    return count
+};
