959160ef0dc70b619144e716806ae884cfe4e7df
Chris KH Han <chris.kh.han@gmail.com>
2021-04-05 23:34:26 -0500

    update

---
+++ 198_houseRobber.js
@@ -0,0 +1,22 @@
+var rob = function(nums) {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) {
+        return Math.max(nums[0], nums[1]);
+    }
+    
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
+    
+
+    for (let i = 2; i < nums.length; i++) {
+        
+        let maxAtCurrent = Math.max(maxAtTwoBefore + nums[i], maxAtOneBefore);
+
+        
+        maxATTwoBefore = maxAtOneBefore;
+        maxOneBefore = maxAtCurrent;
+    }
+
+    return maxOneBefore;
+};
