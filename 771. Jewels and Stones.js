077254e9e505d3310dfc62dbb39fd610b5e96929
Chris Han <chris.kh.han@gmail.com>
2021-06-15 22:40:14 -0500

    update lc 771

---
+++ 771. Jewels and Stones.js
@@ -0,0 +1,13 @@
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+
+    for (let stone of stones) {
+        for (let jewel of jewels) {
+            if (stone == jewel) {
+                count++
+            }
+        }
+    }
+    
+    return count;
+};
