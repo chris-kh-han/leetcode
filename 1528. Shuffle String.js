5bc55b088d19008d85b7f41b74da6139fd535b6a
Chris Han <chris.kh.han@gmail.com>
2021-06-19 22:50:12 -0500

    update lc 1528

---
+++ 1528. Shuffle String.js
@@ -0,0 +1,9 @@
+var restoreString = function(s, indices) {
+    let arr = [];
+    
+    for (let i = 0; i < indices.length; i++) {
+        arr[indices[i]] = s[i]
+    }
+    
+    return arr.join('');
+};
