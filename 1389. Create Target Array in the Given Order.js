ab1b007cfba86019c6dfeda7a8aa7ce856b443d0
Chris Han <chris.kh.han@gmail.com>
2021-07-03 23:00:08 -0500

    lc update #1389

---
+++ 1389. Create Target Array in the Given Order.js
@@ -0,0 +1,8 @@
+var createTargetArray = function(nums, index) {
+    let arr = [];
+    for (let i = 0; i < nums.length; i++) {
+        arr.splice(index[i],0,nums[i]);
+    }
+    
+    return arr;
+};
